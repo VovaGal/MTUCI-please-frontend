a9eae799b06b3d8c8a56d8b6fe2739722e873527
Rustam Shamsutdinov <sypoo@pop-os.localdomain>
2023-11-06 02:52:47 +0300

    try to add registration/login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Aaa from "./pages/Aaa";
 import Lvl1 from "./pages/Lvl1";
 
