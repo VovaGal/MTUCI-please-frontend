dd1bc26c230aaec725afd6afae7b8ef4295360ff
VovaGal <vovagal.7@mail.ru>
2023-11-05 17:41:01 +0300

    dev created

--- src/model/table.js
+++
@@ -1,9 +0,0 @@
-class Table {
-    constructor(x, y, canvasCoordinates, document) {
-        this.x = x
-        this.y = y
-        this.canvasCoordinates = canvasCoordinates /// in pixels
-        this.document = document /// which document present
-    }
-    
-}
---
+++ src/pages/Aaa.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Aaa() {
+    return <h1>Aaa</h1>
+}
